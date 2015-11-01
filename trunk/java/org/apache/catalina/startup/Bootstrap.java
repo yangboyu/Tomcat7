5c3ff72ff410d68f60235a3a13f72a5549e41f36
boyu.yang <boyu.yang@corp.elong.com>
2015-11-01 19:00:25 +0800

    commit

--- trunk/java/org/apache/catalina/startup/Bootstrap.java
+++ trunk/java/org/apache/catalina/startup/Bootstrap.java
@@ -390,7 +390,7 @@ public final class Bootstrap {
      * @param args Command line arguments to be processed
      */
     public static void main(String args[]) {
-
+    	System.out.println("strat !!!");
         if (daemon == null) {
             // Don't set daemon until init() has completed
             Bootstrap bootstrap = new Bootstrap();
